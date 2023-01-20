01ff699fe879297b5fcf080c6d6732e44bce82ae
DuckyBlender <alan@alanklas.com>
2023-01-20 02:22:54 +0100

    removed useless files

--- .svelte-kit/output/client/_app/immutable/chunks/0-66ae441f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/layout.svelte-af06dde3.js";export{t as component};
--- .svelte-kit/output/client/_app/immutable/chunks/2-b7c6a886.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-32ed5336.js";export{t as component};
